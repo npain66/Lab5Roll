207bdde6dda6493d4cf6ffdbaadb83f2b03a0cdb
Nicole D Payne <npain@netscape.com>
2017-02-06 10:09:55 -0500

    first commit

---
+++ src/DiceRoll.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+/**
+ * Created by Nicole on 2/3/2017.
+ */
+public class DiceRoll {
+        public static void main(String[] args) {
+
+            System.out.println("Welcome to the Casino!");
+
+            //1. prompt user for the number sides of dice
+            System.out.println("How many sides should each die have?");
+
+            //2. get user input
+            Scanner scan = new Scanner(System.in);
+            int nextInt = scan.nextInt();
+            int sides = nextInt;
+
+            //3. prompt user to roll the dice
+            System.out.println("Press any number to roll");
+
+            //4. get user input
+            int = switch (6) {
+            }
+
+            //5. perform dice roll/ roll dice
+            int result1 = 2*(sides);//calling a method
+            int result2 = 4*(sides);//calling a method
+
+            //6. Display results
+
+            System.out.println("result1 = " + result1);
+            System.out.println("result2 = " + result2);
+
+            //7. prompt user to continue
+
+            //8. get user choice
+
+
+
+
+
+
+    }
+
+
+}
