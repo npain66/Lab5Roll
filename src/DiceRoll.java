25b9bcc8b93921583e50265f9aac35ac7894055d
Nicole D Payne <npain@netscape.com>
2017-02-06 22:08:11 -0500

    Final

--- src/DiceRoll.java
+++ src/DiceRoll.java
@@ -13,28 +13,40 @@ public class DiceRoll {
 
             //2. get user input
             Scanner scan = new Scanner(System.in);
-            int nextInt = scan.nextInt();
-            int sides = nextInt;
+            int sides = scan.nextInt();
+
+            String procede = "y"; //use for while loop to work
+            while((procede.equals))
 
             //3. prompt user to roll the dice
             System.out.println("Press any number to roll");
 
             //4. get user input
-            int = switch (6) {
+            int =switch (6) {
+                System.out.println();
             }
 
             //5. perform dice roll/ roll dice
-            int result1 = 2*(sides);//calling a method
-            int result2 = 4*(sides);//calling a method
+            int result1 = roll(sides);//calling a method
+            int result2 = roll(sides);//calling a method
 
             //6. Display results
-
             System.out.println("result1 = " + result1);
             System.out.println("result2 = " + result2);
 
             //7. prompt user to continue
+            System.out.println("Would you like to continue y/n?");
+            procede = scan, next();
 
-            //8. get user choice
+        }
+
+    public static void main(String[] args) {
+        //generate random number here ans assign to number variable
+        int = n (int) ((match.random() * sides) = 1);
+        return n;-
+    }
+
+
 
 
 
