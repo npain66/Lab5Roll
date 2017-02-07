748d7890f6bef0837749e1116e93810ebdf8af1f
Nicole D Payne <npain@netscape.com>
2017-02-07 10:57:58 -0500

    Updated

--- src/DiceRoll.java
+++ src/DiceRoll.java
@@ -16,34 +16,33 @@ public class DiceRoll {
             int sides = scan.nextInt();
 
             String procede = "y"; //use for while loop to work
-            while((procede.equals))
+            while (procede.equals("y")) {
 
-            //3. prompt user to roll the dice
-            System.out.println("Press any number to roll");
+                //3. prompt user to roll the dice
+                System.out.println("Press any number to roll");
+                scan.next();
 
-            //4. get user input
-            int =switch (6) {
-                System.out.println();
+                //4. get user input
+                System.out.println("Roll again?");
+
+                //5. perform dice roll/ roll dice
+                int result1 = (sides);//calling a method
+                int result2 = (sides);//calling a method
+
+                //6. Display results
+                System.out.println("result1 = " + result1);
+                System.out.println("result2 = " + result2);
+
+                //7. prompt user to continue
+                System.out.println("Would you like to continue y/n?");
+                procede = scan.next();
             }
-
-            //5. perform dice roll/ roll dice
-            int result1 = roll(sides);//calling a method
-            int result2 = roll(sides);//calling a method
-
-            //6. Display results
-            System.out.println("result1 = " + result1);
-            System.out.println("result2 = " + result2);
-
-            //7. prompt user to continue
-            System.out.println("Would you like to continue y/n?");
-            procede = scan, next();
-
         }
 
-    public static void main(String[] args) {
+    public static int roll(int max) {
         //generate random number here ans assign to number variable
-        int = n (int) ((match.random() * sides) = 1);
-        return n;-
+        int n = (int) ((Math.random() * max) + 1);
+        return n;
     }
 
 
@@ -56,4 +55,4 @@ public class DiceRoll {
     }
 
 
-}
+
